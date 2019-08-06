c638a591ef7ed3762eb8c34a1e3ee736b1087c7b
Nelson <nelsonsprague84@gmail.com>
2019-08-06 14:58:06 -0600

    updating pkg json

---
+++ client/build/precache-manifest.77c79b1b7afa44e8263153deab8b3206.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f67956cacaa3775aec9cc578d27cdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6c6c8c05942bf47d48c",
+    "url": "/static/css/main.dbbd2961.chunk.css"
+  },
+  {
+    "revision": "fa72ed1cbaa48f95cb96",
+    "url": "/static/js/2.0dde0354.chunk.js"
+  },
+  {
+    "revision": "d6c6c8c05942bf47d48c",
+    "url": "/static/js/main.932cc89d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
