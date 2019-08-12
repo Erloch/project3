6c54ab8c7303c2664dcb7c8f6bccf7565392bc32
Erich B <erichbernhardt@hotmail.com>
2019-08-12 15:01:12 -0600

    fixed things

--- client/src/components/CompBtn/index.js
+++ client/src/components/CompBtn/index.js
@@ -1,12 +1,19 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
+import { FaCheck } from "react-icons/fa";
+import ReactTooltip from "react-tooltip";
 
-function CompBtn(props){
-    return(
-        <span className="comp-btn" {...props} role="button" tabIndex="1">
-            $
-        </span>
-    );
+function CompBtn(props) {
+  return (
+    <span className="comp-btn" {...props} role="button" tabIndex="1">
+      <a data-tip data-for="checkFace">
+        <FaCheck className="hvr-grow-shadow" />
+      </a>
+      <ReactTooltip id="checkFace" type="info">
+        <span>Mark Complete</span>
+      </ReactTooltip>
+    </span>
+  );
 }
 
 export default CompBtn;
--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./style.css";
+import { FaRegHandPeace } from "react-icons/fa";
+import ReactTooltip from "react-tooltip";
 
 function DeleteBtn(props) {
   return (
     <span className="delete-btn" {...props} role="button" tabIndex="0">
-      ✗
+    <a data-tip data-for="deleteFace">
+        <FaRegHandPeace className="hvr-grow-shadow" />
+      </a>
+      <ReactTooltip id="deleteFace" type="danger">
+        <span>Not Interested</span>
+      </ReactTooltip>
     </span>
   );
 }
 
 export default DeleteBtn;
+
